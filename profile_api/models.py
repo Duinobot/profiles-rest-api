f3eb6734d1412fcb11064e61c1b894ad6a25541e
Superdan <danmo2060@gmail.com>
2020-01-01 13:51:15 +1100

    fix bugs

--- profile_api/models.py
+++ profile_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManager(BaseUserManager):
     
     def create_superuser(self, email, name, password):
         """create and save superuser"""
-        self.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
